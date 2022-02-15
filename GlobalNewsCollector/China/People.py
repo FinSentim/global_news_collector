bd27411524d55225a955f7367a3605af2268c152
Martin Lindefors <lindefor@kth.se>
2022-02-15 12:13:17 +0100

    Started working with get_articles_list

--- GlobalNewsCollector/China/People.py
+++ GlobalNewsCollector/China/People.py
@@ -25,10 +25,13 @@ class People(BaseCollector.BaseCollector):
                 - Publisher url
         """
         r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'html5lib')
+        soup = BeautifulSoup(r.content, 'html5lib').find('div', attrs={'class': 'layout rm_txt cf'})
         articleInfo = {}
         # Retrive information under title and extract date published
-        date_source_info = soup.find('div', attrs={'class': 'col-1-1 fl'}).text.strip().split("|")
+        try:
+            date_source_info = soup.find('div', attrs={'class': 'col-1-1 fl'}).text.strip().split("|")
+        except AttributeError:
+            date_source_info = soup.find('div', attrs={'class': 'col-1-1'}).text.strip().split("|")
         articleInfo['date_published'] = date_source_info[0]
         articleInfo['date_retrieved'] = date.today().strftime("%d-%m-%Y")
         articleInfo['url'] = url
@@ -38,7 +41,11 @@ class People(BaseCollector.BaseCollector):
             articleInfo['author'] = soup.find('div', attrs={'class':'author cf'}).text 
         except AttributeError:
             articleInfo['author'] = 'N/A'    
-        articleInfo['title'] = soup.find('div', attrs={'class':'col col-1 fl'}).find('h1').text
+        try:
+            articleInfo['title'] = soup.find('div', attrs={'class':'col col-1 fl'}).find('h1').text
+        except AttributeError:
+            articleInfo['title'] = soup.find('div', attrs={'class':'col col-1'}).find('h1').text
+
         articleInfo['publisher'] = "Central Committee of the Chinese Communist Party"
         articleInfo['publisher_url'] = "http://www.people.com.cn" 
 
@@ -63,15 +70,19 @@ class People(BaseCollector.BaseCollector):
             url: The url of the website.
         Returns: A list containing a dictionary returned from get_article() for each article.
         """
-
         r = requests.get(url) 
         soup = BeautifulSoup(r.content, 'html5lib')       
         articleList = []
-        listOFArticles = soup.find('div', attrs={'class':'news_list'})
-        for article in listOFArticles.find_all('dl'):
-            par = article.find('p').find('a', href=True)['href']
+
+        # Start extraxting articles from "hotspot" - section
+        listOFArticles = soup.find('div', attrs={'class':'list2 cf'})
+        for article in listOFArticles.find_all('li'):
+            par = article.find('a', href=True)['href']
+            print(par)
             articleList.append(self.get_article(par))
+            
+        # print(articleList)
         return articleList
 
 p = People()
-p.get_article("http://world.people.com.cn/n1/2022/0215/c1002-32352004.html")
+p.get_articles_list("http://www.people.com.cn/")
