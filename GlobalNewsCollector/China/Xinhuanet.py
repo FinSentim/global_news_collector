5c101d6ad9180ab89fecd0e61eee9cffecb78a11
Vendela Asplund <vasplund@kth.se>
2022-02-16 16:54:26 +0100

    completed issue 5, xinhuanet and made tests

--- GlobalNewsCollector/China/Xinhuanet.py
+++ GlobalNewsCollector/China/Xinhuanet.py
@@ -6,45 +6,43 @@ from datetime import date, datetime, timezone
 class Xinhuanet(metaclass=ABCMeta):
 
     def get_article(self,url:str) -> dict:
-        article_dict = {
-
-        }
+        article_dict = {}
         r = requests.get(url)
         soup = BeautifulSoup(r.content,'html5lib')
         article = ""
 
-        # finding the body of a specific article
-
+        # Finding the body of a specific article
         table = soup.find('div', attrs = {'id':'detail'})
         if(table != None):
-                # for row in table.findAll('div', attrs = {'class':'probe_mc_important'}):
                 for row in table.findAll('p'):
                         article = article + row.text
         
         
 
-        # #Remove unnecessary characters and whitespaces from string
+        # Remove unnecessary characters and whitespaces from string.
         article = article.replace("\n","")
         article = article.replace("\"", "'")
         article = article.replace("\u3000","")
         article = article.strip()
-        
+
+        if(article == ""):
+                raise Exception("Empty article") # Don't add if body of article is empty. 
         try:    
                 author = soup.find('meta',attrs={'property':'article:author'})
                 author = author['content']
         except:
-                # finding the editor of an article
+                # Finding the editor of an article.
                 author = soup.find('span',attrs={"class":"editor"}) 
                 author = author.text
-                # formatting so we only retrieve the name
+                # Formatting to only retrieve the name of the editor.
         
                 author = author.split(":",1)[1]
-                author = author.replace("】","")
+                author = author.replace("】","") # Clean up. 
 
         author = author.replace("\n","")
 
 
-        # getiing date published for article
+        # Getting date published for article.
         date_published = soup.find('div',attrs={"class":"info"})
         date_published = date_published.text[0:20]
         date_published = date_published.replace("\n","")
@@ -53,10 +51,10 @@ class Xinhuanet(metaclass=ABCMeta):
         title = title.text.split("-",1)[0]
         title = title.replace("\n","")
         
-        # # getting todays date
+        # Getting todays date.
         date_retrieved = date.today().strftime("%Y-%m-%d") 
         
-        #adding everything to a dictionary
+        # Adding everything to a dictionary.
         article_dict["date_published"] = date_published
         article_dict["date_retrieved"] = date_retrieved
         article_dict["url"] = url
@@ -75,6 +73,7 @@ class Xinhuanet(metaclass=ABCMeta):
         r = requests.get(url)
         soup = BeautifulSoup(r.content,'html5lib')
 
+        # Adding dictionary for each article to the list
         articles = soup.find('body')
         for row in articles.findAll('div'):
                 try:
