6820e44e1b7dceccca9bf2ce32f9d4069598e2a3
Oscar Backström <oscar-backstrom@hotmail.com>
2022-04-07 10:31:41 +0200

    scrap by tree function implementet

--- general/statpage.py
+++ general/statpage.py
@@ -7,35 +7,69 @@ from readabilipy import simple_tree_from_html_string
 import lxml.html.clean
 
 # req = requests.get('http://world.people.com.cn/n1/2022/0404/c1002-32391390.html')
-req = requests.get('https://www.bhaskar.com/')
+# req = requests.get('https://www.bhaskar.com/')
+req = requests.get('https://www.bhaskar.com/local/uttar-pradesh/agra/news/golden-opportunity-to-make-career-in-hospitality-sector-apply-for-admission-in-heritage-institute-of-hotel-and-tourism-agra-129614083.html')
+# req = requests.get('https://www.svd.se/naringsliv/motor')
+# req = requests.get('https://www.aftonbladet.se/nyheter/a/ALV5Pn/ryska-elever-anger-larare-som-kallar-kriget-for-krig')
 # Fix encoding for non-latin characters
 # Changes the encoding for the webpage from utf-8 to GBK
-# req.raise_for_status()
+req.raise_for_status()
+req.encoding = req.apparent_encoding
 # req.encoding = "GBK"
 
-soup = BeautifulSoup(req.content, 'html5lib')
+# soup = BeautifulSoup(req.content, 'html5lib')
 
 
-print()
+def get_article_bytree(url: str):
+    # make http request
+    r = requests.get(url)
 
-# test = lxml.html.clean.clean_html('<div><li class="_24e83f49e54ee612"><a href="/db-original/news/mlas-increased-by-900-voters-1000-and-mps-15000-know-how-bjp-reached-here-from-its-first-election-129617639.html"><div class="_02462573" title="BJP का 42 साल का सफर: विधायक 900%, वोटर्स 1000% और सांसद 15000% बढ़े, जानिए अपने पहले चुनाव से BJP यहां तक कैसे पहुंची?"><h3><span style="color: rgb(248, 156, 29);">BJP का 42 साल का सफर:</span> विधायक 900%, वोटर्स 1000% और सांसद 15000% बढ़े, जानिए अपने पहले चुनाव से BJP यहां तक कैसे पहुंची?</h3><figure><picture class="_88c1764a"><source media="(max-width: 768px)" type="image/webp" srcset="https://images.bhaskarassets.com/webp/thumb/256x0/web2images/521/2022/04/05/bjp-sthapna-diwas-timeline-6th-april-cover-2_1649176639.jpg"><source media="(min-width: 768px)" type="image/webp" srcset="https://images.bhaskarassets.com/webp/thumb/256x0/web2images/521/2022/04/05/bjp-sthapna-diwas-timeline-6th-april-cover-2_1649176639.jpg"><img src="https://images.bhaskarassets.com/web2images/521/2022/04/05/bjp-sthapna-diwas-timeline-6th-april-cover-2_1649176639.jpg" alt="विधायक 900%, वोटर्स 1000% और सांसद 15000% बढ़े, जानिए अपने पहले चुनाव से BJP यहां तक कैसे पहुंची?|DB ओरिजिनल,DB Original - Dainik Bhaskar" loading="lazy" class="e86bf44b " width="360" height="270"></picture><div style="display: inline-block; align-items: center; justify-content: center;" class="_986e40ac"><svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M23.9998 3.99999C12.9998 3.99999 3.99976 13 3.99976 24C3.99976 35 12.9998 44 23.9998 44C34.9998 44 43.9998 35 43.9998 24C43.9998 13 34.9998 3.99999 23.9998 3.99999Z" fill="var(--background-color)" fill-opacity="0.7"></path><path d="M31.4 22.6L20.4 16.2C19.4 15.6 18 16.4 18 17.6V30.2C18 31.4 19.4 32.2 20.4 31.6L31.4 25.2C32.4 24.8 32.4 23.2 31.4 22.6Z" fill="var(--foreground-color)"></path><path d="M4.1001 24.0002C4.1001 13.0563 13.0562 4.10017 24.0001 4.10017C34.944 4.10017 43.9001 13.0563 43.9001 24.0002C43.9001 34.944 34.944 43.9002 24.0001 43.9002C13.0562 43.9002 4.1001 34.944 4.1001 24.0002Z" stroke="var(--foreground-color)"></path></svg></div></figure></div></a><div class="ba7566ea"><div><span class="_860009df"><a href="/db-original/">DB ओरिजिनल</a></span></div><ul class="_044f5b5b b8e0b8e7"><li class="_55209b5f _978d7e9d"><button title="विधायक 900%, वोटर्स 1000% और सांसद 15000% बढ़े, जानिए अपने पहले चुनाव से BJP यहां तक कैसे पहुंची?" aria-label="facebook" class="react-share__ShareButton" style="background-color: transparent; border: medium none; padding: 0px; font: inherit; color: inherit; cursor: pointer;"><strong></strong></button></li><li class="_55209b5f fd7bcd8c"><button aria-label="twitter" class="react-share__ShareButton" style="background-color: transparent; border: medium none; padding: 0px; font: inherit; color: inherit; cursor: pointer;"><strong></strong></button></li><li class="_55209b5f e5660928"><strong></strong><span>कॉपी लिंक</span></li></ul><div class="df5ade13"><button aria-label="whatsapp" class="react-share__ShareButton a02745d6 _5b92cdac" style="background-color: transparent; border: medium none; padding: 0px; font: inherit; color: inherit; cursor: pointer;"><strong></strong></button>शेयर</div></div></li></div>')
-
-# print(test)
-
-# clean = lxml.html.clean.clean_html(req.text)
-
-# print(clean)
-
-# print(req.encoding)
+    # fix encoding to handle different langauages
+    # r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    # r.encoding = r.apparent_encoding
 
 
-# article = simple_json_from_html_string(req.text, use_readability=False)
+    # Send response thruog readabilipy
+    tree = simple_tree_from_html_string(r.text)
+    print(tree.prettify())
+    title = ""
+    article = ""
 
-# s = article['title']
-# p = article['plain_content']
+    # Try to fetch the title
+    for h1 in tree.find_all('h1'):
+        title = h1
+        # print(h1.text)
+
+    if title == "":
+        for h2 in tree.find_all('h2'):
+            title = h2
+            # print(h2.text)
+    
+    for row in tree.findAll('p'):
+        article = article + row.text
+
+    
+
+    # print(article)
+    
+
+
+get_article_bytree('http://world.people.com.cn/n1/2022/0404/c1002-32391390.html')
+# get_article_bytree('https://www.aftonbladet.se/nyheter/a/ALV5Pn/ryska-elever-anger-larare-som-kallar-kriget-for-krig')
+# print(" ")
+# get_article_bytree('https://www.theguardian.com/world/2022/apr/07/turkish-court-sends-case-of-26-accused-over-khashoggi-killing-to-saudi-arabia')
+# print(" ")
+# get_article_bytree('https://www.manager-magazin.de/finanzen/bundesbank-praesident-joachim-nagel-glaubt-an-baldigen-zinsanstieg-a-58d5345b-db65-4262-ad46-4e447eb955a2')
+# get_article_bytree('https://hindi.business-standard.com/storypage.php?autono=186301')
+# article = simple_json_from_html_string(req.text, use_readability=True)
+
+# # s = article['title']
+# # p = article['plain_content']
 # t = article['plain_text']
+# print(t)
 
-tree = simple_tree_from_html_string(req.text)
+# tree = simple_tree_from_html_string(req.text)
 
 # print(tree.prettify())
 # print(BeautifulSoup(req.content, 'html5lib'))
