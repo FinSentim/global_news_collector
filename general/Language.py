49b4651e3adab5711f9f919a934bca0f0ba6f1ec
Oscar Backström <oscar-backstrom@hotmail.com>
2022-04-19 08:30:03 +0200

    remove oscars test files

--- general/Language.py
+++
@@ -1,4 +0,0 @@
-from lingua import Language, LanguageDetectorBuilder
-languages = [Language.ENGLISH, Language.FRENCH, Language.GERMAN, Language.SPANISH]
-detector = LanguageDetectorBuilder.from_languages(*languages).build()
-print(detector.detect_language_of("languages are awesome"))
